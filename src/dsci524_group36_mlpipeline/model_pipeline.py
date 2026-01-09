abe3207fe8e69ac63a1b1ac69ac5d79cd46fbe6e
Claudia <87799252+claudia-liauw@users.noreply.github.com>
2026-01-09 10:34:06 -0800

    docstring for pipeline function

---
+++ src/dsci524_group36_mlpipeline/model_pipeline.py
@@ -0,0 +1,36 @@
+def create_model_pipeline(X, numerical_feat, categorical_feat, model='lr'):
+    """
+    Create model pipeline for model of choice (logistic regression, SVC or
+    random forest) with standardisation for numerical features and one-hot
+    encoding for categorical features. Any remaining features are passed
+    through.
+
+    Parameters
+    ----------
+    X : pd.DataFrame
+        Data without target column.
+    numerical_feat : list
+        Names of numerical columns to be standardised.
+    categorical_feat : list
+        Names of categorical columns to be one-hot encoded.
+    model : {'lr', 'svc', 'rf'}, default = 'lr'
+        Model to include in pipeline.
+        `lr`: `LogisticRegression`
+        `svc`: `SVC`
+        `rf`: `RandomForestClassifier`
+
+    Returns
+    -------
+    sklearn.pipeline.Pipeline
+        Pipeline with standardisation, one-hot encoding and specified model.
+
+    Raises
+    ------
+    ValueError
+        If input types are wrong or columns are not found in dataframe.
+
+    Examples
+    --------
+    >>> create_model_pipeline(X, ['age'], ['sex'], 'lr')
+    """
+    pass
