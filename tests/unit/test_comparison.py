145c0b084b9034fd911cc65292aafa0dddc2cf3e
Bright Bello <brightbello04@gmail.com>
2026-01-14 07:18:52 -0800

    added test functions

--- tests/unit/test_comparison.py
+++ tests/unit/test_comparison.py
@@ -7,6 +7,8 @@ from sklearn.linear_model import LogisticRegression, LinearRegression
 from sklearn.ensemble import RandomForestClassifier, ExtraTreesClassifier
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
+import pytest
+from sklearn.base import is_classifier
 
 log = LogisticRegression()
 lr = LinearRegression()
@@ -28,42 +30,54 @@ rf.fit(X_train,y_train)
 
 from dsci524_group36_mlpipeline.model_comparison import model_comparison
 
-def test_metric_type():
+def test_valid_skmetric():
     """
     Tests that the function returns an error if the wrong data type
     is passed for metric.
     """
-    pass
+    metric = 4
+    best = model_comparison([log, rf], X_train, y_train, metric=metric,greater_is_better=False)
 
-def test_valid_skmetric():
-    """
-    Tests that the function raises an error if a non sklearn metric
-    is passed
-    """
-    pass
+    with pytest.raises(TypeError, match="Expected metric input to be str"): 
+        model_comparison([log, rf], X_train, y_train, metric=metric,greater_is_better=False)
 
 def test_warning_non_classifier():
     """
     Tests that the function raises a warning if a non classification model
     is added to the metric list
     """
-    pass
+    metric = "mean_squared_error"
+    
+    with pytest.warns(UserWarning):
+        model_comparison([log, lr], X_train, y_train, metric=metric,greater_is_better=False)
 
 def test_function_output():
     """
     Tests that the output of the function is a valid sklearn classification model
     """
-    pass
+    metric = "mean_squared_error"
+    
+    best = model_comparison([log, rf], X_train, y_train, metric=metric,greater_is_better=False)
+    
+    assert is_classifier(best) == True
+
+        
 
 def test_no_classifier_found():
     """
     Tests that the function raises a value error if no valid classification models are
     passed into the function.
     """
-    pass
+    metric = "mean_squared_error"
+
+    with pytest.raises(ValueError, match="No valid Classification models provided"): 
+        model_comparison([lr], X_train, y_train, metric=metric,greater_is_better=False)
 
 def test_model_is_fitted():
     """
     Tests if the function raises an error if the passed model has not been fitted
     """
-    pass
+    metric = "mean_squared_error"
+    
+    with pytest.warns(UserWarning):
+        model_comparison([log, et], X_train, y_train, metric=metric,greater_is_better=False)
